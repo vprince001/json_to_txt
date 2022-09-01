7288468566452a7861873f30d1f5f414db84e6ad
adalcinojunior <adaljunior.inf@gmail.com>
2022-08-31 23:16:39 -0300

    [feat] compatibility with heterogeneous objects
    Quick explanation: currently the lib only works with objects that have the same set of keys. To solve this problem, I grouped the objects according to their keys before generating the rows with getRows.

--- json_to_txt.js
+++ json_to_txt.js
@@ -2,11 +2,16 @@ const fs = require("fs");
 const { getStartPoints, getRows, getData } = require("./src/lib.js");
 
 const main = function (params) {
-  const data = getData(params, fs);
-  const headers = Object.keys(data[0]);
-  const startPoints = getStartPoints(data, headers);
-  const rows = getRows(data, headers, startPoints);
-  return rows.join("\n");
-};
+    const data = getData(params, fs);
+    const groups = Array.from(new Set(data.map(item => Object.keys(item).toString())));
+    return groups
+        .map(group => {
+            const groupData = data.filter(item => Object.keys(item).toString() === group);
+            const headers = Object.keys(groupData[0]);
+            const startPoints = getStartPoints(groupData, headers);
+            const rows = getRows(groupData, headers, startPoints);
+            return rows.join('\n');
+        }).join('\n');
+}
 
 module.exports = main;
